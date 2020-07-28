2a1acedab09bdc6a64ed1bc58bc997146efcdb90
Carlos Chavez <cvchavez92@gmail.com>
2020-07-28 11:17:00 -0600

    included username to order

--- PizzaStore.Client/Runner.cs
+++ PizzaStore.Client/Runner.cs
@@ -1,27 +1,27 @@
-using System.Collections.Generic;
-using PizzaStore.Domain.Models;
-using PizzaStore.Domain.Models.PizzaModel;
-using PizzaStore.Domain.Models.ToppingModel;
-using PizzaStore.Storing.Repositories;
+// using System.Collections.Generic;
+// using PizzaStore.Domain.Models;
+// using PizzaStore.Domain.Models.PizzaModel;
+// using PizzaStore.Domain.Models.ToppingModel;
+// using PizzaStore.Storing.Repositories;
 
-namespace PizzaStore.Client
-{
-  public class Runner
-  {
-    static void Main(string [] args)
-    {
-      var repository = new PizzaRepository();
-      // System.Console.WriteLine("or here");
-      // var pizza = new Pizza(new Name("My Cheese P"),new Size("M"),new Crust("Regular"),new List<Topping>{new Cheese(), new Jalapeno()});
-      // System.Console.WriteLine("here");
-      // var pizza = new CheesePizzaFactory();
-      // repository.Create(pizza);
-      var pizzaList = repository.ReadAll();
-      foreach(var p in pizzaList)
-      {
-        System.Console.WriteLine(p);
-        System.Console.WriteLine();
-      }
-    }
-  }
-}
+// namespace PizzaStore.Client
+// {
+//   public class Runner
+//   {
+//     static void Main(string [] args)
+//     {
+//       var repository = new PizzaRepository();
+//       // System.Console.WriteLine("or here");
+//       // var pizza = new Pizza(new Name("My Cheese P"),new Size("M"),new Crust("Regular"),new List<Topping>{new Cheese(), new Jalapeno()});
+//       // System.Console.WriteLine("here");
+//       // var pizza = new CheesePizzaFactory();
+//       // repository.Create(pizza);
+//       var pizzaList = repository.ReadAll();
+//       foreach(var p in pizzaList)
+//       {
+//         System.Console.WriteLine(p);
+//         System.Console.WriteLine();
+//       }
+//     }
+//   }
+// }
